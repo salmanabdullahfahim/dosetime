897906baf53f1e3a592518f15fc856ceb4457b3c
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2025-02-15 13:18:44 +0600

    Enhance SplashScreen with animated fade and scale effects

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,34 @@ import { Ionicons } from "@expo/vector-icons";
 import { useEffect, useRef } from "react";
 
 export default function SplashScreen() {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.5)).current;
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.spring(scaleAnim, {
+        toValue: 1,
+        tension: 10,
+        friction: 2,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Splash Screen</Text>
+      <Animated.View
+        style={[
+          styles.iconContainer,
+          { opacity: fadeAnim, transform: [{ scale: scaleAnim }] },
+        ]}
+      >
+        <Ionicons name="medical" size={100} color={"#fff"} />
+        <Text style={styles.appName}>DoseTime</Text>
+      </Animated.View>
     </View>
   );
 }
@@ -16,5 +41,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#4CAF50",
     justifyContent: "center",
     alignItems: "center",
+  },
+  iconContainer: {
+    alignItems: "center",
+  },
+  appName: {
+    color: "#fff",
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 20,
+    letterSpacing: 1,
   },
 });
